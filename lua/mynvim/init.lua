c1f6513ffe7dc5947566d5f1082cbd87be4ce0ae
Pietro Giorgi <pietro.tgiorgi@gmail.com>
2024-10-04 16:02:48 -0400

    update <leader>pv

--- lua/mynvim/init.lua
+++ lua/mynvim/init.lua
@@ -1,4 +1,13 @@
 require("mynvim.packer")
 require("mynvim.set")
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", function()
+    local current_buf = vim.api.nvim_buf_get_name(0)
+
+    if current_buf == "" or vim.bo.filetype == "netrw" then
+        vim.cmd("lcd ..");
+        vim.cmd("Ex ..");
+    else
+        vim.cmd("Ex");
+    end
+end)
